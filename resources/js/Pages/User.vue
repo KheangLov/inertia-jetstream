1c960901c1b8707cdcc2b5efc590d9f8f3cb83db
lovsokheang <lovsokheang@gmail.com>
2021-03-27 17:36:13 +0700

    Add latest

---
+++ resources/js/Pages/User.vue
@@ -0,0 +1,29 @@
+<template>
+    <app-layout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                User
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-md sm:rounded-lg">
+                    <custom-table-component />
+                </div>
+            </div>
+        </div>
+    </app-layout>
+</template>
+
+<script>
+    import AppLayout from '@/Layouts/AppLayout';
+    import CustomTableComponent from '@/Custom/TableComponent';
+
+    export default {
+        components: {
+            AppLayout,
+            CustomTableComponent
+        },
+    }
+</script>
